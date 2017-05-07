1e0726a131ef713da915005b7cce055083df486e
Daniel <daniel.sarmiento.r@gmail.com>
2017-05-07 22:23:41 +0200

    Challenge 9 - Mejorado y subido

---
+++ challenge-9/tc-9.py
@@ -0,0 +1,43 @@
+import os
+import time
+import math
+
+in_file_path = os.path.join(os.path.dirname(__file__), "submitInput.txt")
+out_file_path = os.path.join(os.path.dirname(__file__), "testOutput.txt")
+
+start = time.time()
+with open(in_file_path, 'r') as infile:
+    with open(out_file_path, 'w') as outfile:
+        cases = int(infile.readline())
+
+        for case in range(cases):
+            sections = infile.readline().split()
+            s = int(sections[0])  # Straight sections
+            c = int(sections[1])  # 90 deg curves
+            d = int(sections[2])  # Double straights
+
+            used_c = math.trunc(c / 2) * 2
+
+            if used_c < 4:
+                used_c = 0
+                used_s = 0
+                used_d = 0
+            elif used_c == 4:
+                used_s = math.trunc(s / 2) * 2
+                if s >= 2:
+                    used_d = d
+                else:
+                    used_d = math.trunc(d / 2) * 2
+            elif (used_c - 2) % 4 == 0 and s >= 2:
+                used_s = math.trunc(s / 2) * 2
+                used_d = d
+            elif used_c == 8 and (s >= 2 or d >= 1):
+                used_s = math.trunc(s / 2) * 2
+                used_d = d
+            elif used_c >= 12 and used_c % 4 == 0:
+                used_s = math.trunc(s / 2) * 2
+                used_d = d
+
+            used_c -= 2
+            outfile.write("Case #" + str(case + 1) + ": " + str(
+                used_c + used_s + used_d) + "\n")
