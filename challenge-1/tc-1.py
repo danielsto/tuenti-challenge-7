9b22bbec2be2ec1115798b6e3fe8566a071126de
Daniel <daniel.sarmiento.r@gmail.com>
2017-05-08 19:17:52 +0200

    Challenge 1 - Actualizada implementación

--- challenge-1/tc-1.py
+++ challenge-1/tc-1.py
@@ -6,13 +6,12 @@ out_file_path = os.path.join(os.path.dirname(__file__), "testOutput.txt")
 
 with open(in_file_path, 'r') as infile:
     with open(out_file_path, 'w') as outfile:
-        cases = infile.readline()
-        for case in range(int(cases)):
-            case_n = " ".join(infile.readline().split())
-            hunger_1 = (infile.readline().split())
-            slices = 0
-            for sliced in hunger_1:
-                slices = slices + int(sliced)
-            pizzas = slices / 8
-            outfile.write("Case #" + str(case + 1) + ": " + str(
-                math.ceil(pizzas)) + "\n")
+        cases = int(infile.readline())
+
+        for case in range(cases):
+            next(infile)
+            hunger = list(map(int, (infile.readline().split())))
+            slices = sum(hunger)
+            pizzas = math.ceil(slices / 8)
+
+            outfile.write("Case #" + str(case + 1) + ": " + str(pizzas) + "\n")
