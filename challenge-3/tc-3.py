8a7e29589a750715b8ae0b02e21cc573a4a3e86d
Daniel <daniel.sarmiento.r@gmail.com>
2017-05-09 10:20:43 +0200

    Challenge 3 - optimizado y probado

--- challenge-3/tc-3.py
+++ challenge-3/tc-3.py
@@ -5,8 +5,11 @@ out_file_path = os.path.join(os.path.dirname(__file__), "testOutput.txt")
 
 with open(in_file_path, 'r') as infile:
     with open(out_file_path, 'w') as outfile:
+
         cases = infile.readline()
+
         for case in range(int(cases)):
             points = int(infile.readline())
-            cards = math.floor(math.log(points, 2)) + 1
+            cards = math.ceil(math.log(points, 2))
+
             outfile.write("Case #" + str(case + 1) + ": " + str(cards) + "\n")
