b5589186de3addc6f9885a03c7e35a92857d0a92
Daniel <daniel.sarmiento.r@gmail.com>
2017-05-09 12:38:28 +0200

    Challenge 4 - mejorada legibilidad

--- challenge-4/tc-4.py
+++ challenge-4/tc-4.py
@@ -8,7 +8,9 @@ out_file_path = os.path.join(os.path.dirname(__file__), "testOutput.txt")
 start = time.time()
 with open(in_file_path, 'r') as infile:
     with open(out_file_path, 'w') as outfile:
+
         cases = int(infile.readline())
+
         for case in range(cases):
             sides = [int(i) for i in infile.readline().split()]
             min_perimeter = 0
@@ -18,11 +20,13 @@ with open(in_file_path, 'r') as infile:
                 if a + b > c and b + c > a and a + c > b:
                     min_perimeter = a + b + c
                     break
+
             if min_perimeter == 0:
                 outfile.write(
                     "Case #" + str(case + 1) + ": " + "IMPOSSIBLE" + "\n")
             else:
                 outfile.write("Case #" + str(case + 1) + ": " + str(
                     min_perimeter) + "\n")
+
         runtime = time.time() - start
         print(runtime)
